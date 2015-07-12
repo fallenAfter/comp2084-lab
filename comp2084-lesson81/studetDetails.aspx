f5c53566298a90d63f533a5f0ec82d8eaee2972c
fallenAfter <brandon.6elliott@gmail.com>
2015-07-12 13:53:35 -0400

    Added CRUD ability for a students table

---
+++ comp2084-lesson81/studetDetails.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/site.Master" AutoEventWireup="true" CodeBehind="studetDetails.aspx.cs" Inherits="comp2084_lesson81.studetDetails" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Student Details</h1>
+
+    <div class="form-group">
+        <label for="txtFName" class="col-sm-2">First Name</label>
+        <asp:TextBox ID="txtFName" runat="server" MaxLength="50"></asp:TextBox>
+    </div>
+
+    <div class="form-group">
+        <label for="txtLName" class="col-sm-2">First Name</label>
+        <asp:TextBox ID="txtLName" runat="server" MaxLength="50"></asp:TextBox>
+    </div>
+
+    <div class="form-group">
+        <asp:Button runat="server" ID="btnSave" text="Save" CssClass="btn btn-primary" OnClick="btnSave_Click" />
+    </div>
+
+</asp:Content>
---
+++ comp2084-lesson81/studetDetails.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//add database to resource list
+using comp2084_lesson81.Models;
+
+namespace comp2084_lesson81
+{
+    public partial class studetDetails : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //call post back so the form does not update with existing information
+            if (!IsPostBack)
+            {
+                //look up selected department from get to fill in form
+                if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+                {
+                    getStudent();
+                }
+            }
+        }
+
+        protected void getStudent()
+        {
+            //fill in form based upon selected student
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                Int32 StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+                //search departmet
+                Student stdt = (from s in db.Students
+                                where s.StudentID == StudentID
+                                select s).FirstOrDefault();
+
+                //populate fields
+                txtLName.Text = stdt.LastName;
+                txtFName.Text = stdt.FirstMidName;
+
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            //connect to database
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                //create new student in memory
+                Student stdt = new Student();
+                Int32 StudentID = 0;
+
+                 DateTime EnDate = DateTime.Now;
+
+                //handle updating a record
+                if (!String.IsNullOrEmpty(Request.QueryString["StudentID"]))
+                {
+                    StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+
+                    stdt = (from s in db.Students
+                            where s.StudentID == StudentID
+                            select s).FirstOrDefault();
+
+                    //set eroll date to equal existing result
+                    EnDate = stdt.EnrollmentDate;
+
+
+                }
+
+                //fill in student details
+                stdt.FirstMidName = txtLName.Text;
+                stdt.LastName = txtFName.Text;
+                //add current date time as time of registering
+                stdt.EnrollmentDate = EnDate;
+
+                if (StudentID == 0)
+                {
+                    //create new row in database
+                    db.Students.Add(stdt);
+                }
+
+                db.SaveChanges();
+
+                //redirect back to students page
+                Response.Redirect("students.aspx");
+
+
+            }
+
+        }
+    }
+}
